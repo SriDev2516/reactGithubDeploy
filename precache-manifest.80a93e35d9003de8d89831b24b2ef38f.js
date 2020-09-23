b651c37ffe87e89fc70e18753248c3bede64a7e7
Srinivas Annavarpu <srinivas.net2516@gmail.com>
2020-09-23 14:03:29 +0530

    Updates

---
+++ precache-manifest.80a93e35d9003de8d89831b24b2ef38f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8d450175330760091c34aba21942cf",
+    "url": "/reactGithubDeploy/index.html"
+  },
+  {
+    "revision": "5e0a8e0d4475efc76aa5",
+    "url": "/reactGithubDeploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5e01756cc23aecea81d2",
+    "url": "/reactGithubDeploy/static/js/2.783a38ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactGithubDeploy/static/js/2.783a38ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e0a8e0d4475efc76aa5",
+    "url": "/reactGithubDeploy/static/js/main.77fc7b7c.chunk.js"
+  },
+  {
+    "revision": "6c2aea70502ba89d4902",
+    "url": "/reactGithubDeploy/static/js/runtime-main.adec33b2.js"
+  }
+]);
